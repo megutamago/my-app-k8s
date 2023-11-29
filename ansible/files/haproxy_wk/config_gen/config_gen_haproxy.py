73a845e776a9aac9f1ddd08f19934597484dfff5
megutamago <naoya05280708@gmail.com>
2023-11-29 23:07:55 +0900

    submit

--- ansible/files/haproxy_wk/config_gen/config_gen_haproxy.py
+++ ansible/files/haproxy_wk/config_gen/config_gen_haproxy.py
@@ -5,9 +5,13 @@ WK_IP_2 = '192.168.11.122'
 WK_IP_3 = '192.168.11.123'
 
 lists = {
-    # id          # service   # IP               # port    # nodeport
-    'service1': [ 'argocd',   '192.168.11.140',  '443',    '31000'   ],
-    'service2': [ 'grafana',  '192.168.11.141',  '80',     '31001'   ],
+    # id          # service      # IP               # port    # nodeport
+    'service1': [ 'argocd',      '192.168.11.140',  '443',    '31000' ],
+    'service2': [ 'grafana',     '192.168.11.141',  '80',     '31001' ],
+    'service3': [ 'prometheus',  '192.168.11.142',  '9090',   '31002' ],
+    'service4': [ 'minio',       '192.168.11.143',  '80',     '31004' ],
+    'service5': [ 'loki',        '192.168.11.144',  '3100',   '31005' ],
+    'service6': [ 'tempo',       '192.168.11.145',  '4317',   '31006' ],
 }
 
 # config gen haproxy
@@ -24,4 +28,4 @@ def generate_script(template_path, output_path, **kwargs):
     with open(output_path, 'w') as output_file:
         output_file.write(rendered_script)
 
-generate_script('../haproxy/template.cfg', '../haproxy/test.cfg', lists=lists, WK_IP_1=WK_IP_1, WK_IP_2=WK_IP_2, WK_IP_3=WK_IP_3)
+generate_script('../haproxy/template.cfg', '../haproxy/haproxy.cfg', lists=lists, WK_IP_1=WK_IP_1, WK_IP_2=WK_IP_2, WK_IP_3=WK_IP_3)
--- ansible/files/haproxy_wk/config_gen/config_gen_keepalived.py
+++ ansible/files/haproxy_wk/config_gen/config_gen_keepalived.py
@@ -7,6 +7,10 @@ lists = {
     # id          # LB_ID  # vrId  # IP             
     'service1': [ '1',     '61',   '192.168.11.140' ],
     'service2': [ '2',     '62',   '192.168.11.141' ],
+    'service3': [ '3',     '63',   '192.168.11.142' ],
+    'service4': [ '4',     '64',   '192.168.11.143' ],
+    'service5': [ '5',     '65',   '192.168.11.144' ],
+    'service6': [ '6',     '66',   '192.168.11.145' ],
 }
 
 # config gen keepalived
@@ -23,4 +27,4 @@ def generate_script(template_path, output_path, **kwargs):
     with open(output_path, 'w') as output_file:
         output_file.write(rendered_script)
 
-generate_script('../keepalived/template.conf', '../keepalived/test.conf', lists=lists, WK_IP_1=WK_IP_1, WK_IP_3=WK_IP_3)
+generate_script('../keepalived/template.conf', '../keepalived/keepalived.conf', lists=lists, WK_IP_1=WK_IP_1, WK_IP_3=WK_IP_3)
